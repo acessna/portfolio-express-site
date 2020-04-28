e52e50f1fdefc25026ec0d9b1d095dd654087741
Adam Cessna <racessna@gmail.com>
2020-04-28 17:23:23 -0500

    Add routes to the home, about and project pages. Edit the informatiin in the pug files

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const path = require('path');
 const port = 3000;
 
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('project');
+});
 
 
 
