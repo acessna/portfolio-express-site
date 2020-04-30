8baa8a7a553aac570590b04be05115f231be484c
Adam Cessna <racessna@gmail.com>
2020-04-29 19:01:41 -0500

    Edit JSON file to add image links, edit about page with information, edit index to create thumbnails for each project. Static images folder does not seem to be loading

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
-const path = require('path');
 const port = 3000;
 
 app.use('/static', express.static('public'));
@@ -10,7 +9,7 @@ app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { data} );
 });
 
 app.get('/about', (req, res) => {
@@ -18,7 +17,7 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('project');
+  res.render('project', data);
 });
 
 
